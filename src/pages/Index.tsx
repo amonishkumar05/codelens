6e480a85685d52fc399537d740a26aae540cb07d
rahuletto <rahulmarban@gmail.com>
2025-04-20 04:26:10 +0530

    style: home page changes

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -6,11 +6,11 @@ const Index = () => {
 
 
   return (
-    <div className="min-h-screen flex flex-col">
+    <div className="h-screen flex flex-col overflow-hidden">
       <GradientHeader
       />
 
-      <div className="container mx-auto max-w-screen-2xl flex-row flex gap-5 justify-between px-4 py-8 flex-1">
+      <div className="container mx-auto max-w-screen-xl flex-row flex gap-5 justify-between px-4 py-8 flex-1">
 
         <div className="flex flex-col gap-4">
           <Pixelate className="text-6xl font-regular mb-2 text-left pixel">
@@ -22,9 +22,11 @@ const Index = () => {
           <a href="/review" className="text-black px-7 py-3 hover:px-9 transition-all duration-150 w-fit rounded-full mt-4 font-semibold bg-[#8872df]"><Pixelate className="">Let&apos;s review</Pixelate></a>
         </div>
 
-        <img src="/scan.svg" alt="scan" className=" w-[300px] h-[300px] object-cover opacity-70 z-5" />
+        <img src="/scan.svg" alt="scan" className=" w-[250px] h-[250px] object-cover opacity-70 z-5" />
       </div>
 
+
+      <img src="/review.png" alt="bg" className="object-cover rounded-2xl mt-12 border-4 border-[#8872df]/40 max-w-screen-xl mx-auto z-0" />
     </div>
   );
 };
